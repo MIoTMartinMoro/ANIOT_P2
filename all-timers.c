a615670f39e67796e877dba16f97693a8afb46f0
user-iot <alda.martin.munoz@alumnos.upm.es>
2018-10-16 20:38:20 +0200

    primer intento con procesos

--- all-timers.c
+++ all-timers.c
@@ -36,70 +36,61 @@
 #include "sys/timer.h"
 #include "sys/rtimer.h"
 
-PROCESS(process1, "ETimer x Timer x STimer Process");
-PROCESS(process2, "CTimer Process 2");
-PROCESS(process3, "RTimer Process 3");
-AUTOSTART_PROCESSES(&process1, &process2, &process3);
+#define PROCESS_EVENT_P1a2  0x8b
+#define PROCESS_EVENT_P2a1  0x8c
 
-static int counter_etimer;
-static int counter_timer;
-static int counter_stimer;
-static int counter_ctimer;
-static int counter_rtimer;
+PROCESS(process1, "Event Timer deadline 4 segs");
+PROCESS(process2, "Callback Timer deadline 5 segs");
+AUTOSTART_PROCESSES(&process1, &process2);
+
+static int contador1;
+static int contador2;
+
 static struct timer timer_timer;
 static struct stimer timer_stimer;
 static struct ctimer timer_ctimer;
 static struct rtimer timer_rtimer;
+static struct etimer timer_etimer;
 static rtimer_clock_t timeout_rtimer = RTIMER_SECOND / 2;
 
 void
-do_timeout1()
+do_timeout1(int event)
 {
-  counter_etimer++;
-  if(timer_expired(&timer_timer)) {
-    counter_timer++;
+  if(event==PROCESS_EVENT_P2a1){
+    printf("Proceso 1: %d\n", contador1);
+    contador1++;
+  }else if(event==PROCESS_EVENT_TIMER){
+    process_post_synch(&process2, PROCESS_EVENT_P1a2, NULL);
+    etimer_reset(&timer_etimer);  
   }
-
-  if(stimer_expired(&timer_stimer)) {
-    counter_stimer++;
-  }
-
-  printf("\nProcess 1: %s", counter_timer == counter_etimer
-         && counter_timer == counter_stimer ? "SUCCESS" : "FAIL");
 }
 /*---------------------------------------------------------------------------*/
 void
 do_timeout2()
 {
-  ctimer_reset(&timer_ctimer);
-  printf("\nProcess 2: CTimer callback called");
-  counter_ctimer++;
+    process_post(&process1, PROCESS_EVENT_P2a1, NULL);
+    ctimer_reset(&timer_ctimer);
 }
-/*---------------------------------------------------------------------------*/
 void
-do_timeout3(struct rtimer *timer, void *ptr)
+do_timeout3(int event)
 {
-  counter_rtimer++;
-
-  printf("\nProcess 3: RTimer callback called");
-
-  /* Re-arm rtimer */
-  rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0, do_timeout3,
-             NULL);
+  if(event==PROCESS_EVENT_P1a2){
+    printf("Proceso 2: %d\n", contador2);
+    contador2++;
+  }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(process1, ev, data)
 {
-  static struct etimer timer_etimer;
+  
 
   PROCESS_BEGIN();
 
   while(1) {
-    timer_set(&timer_timer, 3 * CLOCK_SECOND);
-    stimer_set(&timer_stimer, 3);
-    etimer_set(&timer_etimer, 3 * CLOCK_SECOND);
-    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
-    do_timeout1();
+    etimer_set(&timer_etimer, 4 * CLOCK_SECOND);
+    PROCESS_WAIT_EVENT_UNTIL(ev==PROCESS_EVENT_P2a1 || ev== PROCESS_EVENT_TIMER);
+    do_timeout1(ev);
+    
   }
 
   PROCESS_END();
@@ -111,21 +102,10 @@ PROCESS_THREAD(process2, ev, data)
 
   while(1) {
     ctimer_set(&timer_ctimer, 5 * CLOCK_SECOND, do_timeout2, NULL);
-    PROCESS_YIELD();
+    PROCESS_WAIT_EVENT_UNTIL(ev==PROCESS_EVENT_P1a2); 
+    do_timeout3(ev);
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(process3, ev, data)
-{
-  PROCESS_BEGIN();
 
-  while(1) {
-    rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0,
-               do_timeout3, NULL);
-    PROCESS_YIELD();
-  }
-
-  PROCESS_END();
-}
